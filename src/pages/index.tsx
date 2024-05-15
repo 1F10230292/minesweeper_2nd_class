7730524e6592245353e9c2a5e316d95a289e4f0e
ポケモンサトシ <131662898+1F10230292@users.noreply.github.com>
2024-05-15 18:05:08 +0900

    初期bomb配置の際にcell周囲の爆弾count表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,77 @@
 import { useState } from 'react';
 import styles from './index.module.css';
-
+const directions = [
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 const getRandomInt = (min: number, max: number) => {
   // Math.random()は0以上1未満の値を返すため、適切な範囲に変換する
   //min~max間の整数を返す関数を設定
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const defaultBombSeries = (bombMap: number[][]) => {
+  return bombMap.flat();
+};
+const defaultBombCounts = (bombMap: number[][]) => {
+  return defaultBombSeries(bombMap).filter((bomb) => bomb === 11).length;
+};
 // cellをクリックした際に生成されたbombを一時的に記憶する関数
 const plotBomb = (x: number, y: number, bombMap: number[][]) => {
-  const bombX = getRandomInt(0, 8);
+  let bombX = getRandomInt(0, 8);
 
-  const bombY = getRandomInt(0, 8);
+  let bombY = getRandomInt(0, 8);
+  while (defaultBombCounts(bombMap) < 10) {
+    // 被っていっれば変更なしで返す
+    if (bombX === x && bombY === y) {
+      continue;
+    }
+    //そうでなければ爆弾を連ダムにプロットする
+    else {
+      //cellに何もない & タップした場所以外である時にplot可能
+      if (bombMap[y][x] === 0) {
+        bombMap[bombY][bombX] = 11;
+        // 乱数を新しく生成
+        bombX = getRandomInt(0, 8);
 
-  // 被っていっれば変更なしで返す
-  if (bombX === x && bombY === y) {
-    return bombMap;
+        bombY = getRandomInt(0, 8);
+      } else {
+        continue;
+      }
+    }
   }
-  //そうでなければ爆弾を連ダムにプロットする
-  bombMap[bombY][bombX] = 11;
+
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      let bombCounter = 0;
+      if (bombMap[y][x] === 0) {
+        for (const direction of directions) {
+          {
+            if (
+              bombMap[y + direction[0]] !== undefined &&
+              bombMap[y + direction[0]][x + direction[1]] === 11
+            ) {
+              bombCounter += 1;
+
+              //newPlotBomb[y][x] = 1
+            }
+          }
+        }
+        bombMap[y][x] = bombCounter;
+      }
+    }
+  }
+
   return bombMap;
 };
-const Home = () => {
-  const [samplePos, setSamplePos] = useState(0);
-  console.log(samplePos);
 
+const Home = () => {
   const [tapPos, setTapPos] = useState(0);
   // bombの位置を更新し、記憶する関数、useState
   const [bombMap, setBombMap] = useState([
@@ -39,6 +86,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const [userInput, setUserInput] = useState([
+    //右クリックしたときの表示の順番 旗 -> ？ -> 表示なし -> 旗 -> ？ -> 表示なしをクリックする
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -49,13 +97,18 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-
   //cellをクリックした際の挙動の関数
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
-    //bombMapをいじるのはご法度、ゆえにクローンをして、それをいじる。
+    //bombMap本体をいじるのはご法度、ゆえにクローンをして、それをいじる。
     const newMap = structuredClone(bombMap);
     const newPlotBomb = plotBomb(x, y, newMap);
+
+    // const bombCounter = ()=>{
+    //   const toFlat = newPlotBomb.flat();
+    //   toFlat.filter((cellNum)=>cellNum === 11)
+    // }
+
     setBombMap(newPlotBomb);
   };
 
@@ -65,11 +118,18 @@ const Home = () => {
         {userInput.map((row, y) =>
           row.map((spot, x) => (
             //x,yそれぞれ0~8の中から一つずつ選びそれをsetBombMapに代入、しかしuserInputしていないところのみ
-            <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+
+            <div
+              className={styles.cellStyle}
+              key={`${x}-${y}`}
+              onClick={() => clickHandler(x, y)}
+              //クリックした座標に１を代入し、spot=== 1の時visibility: `hidden` にするようにする
+              // style={{ visibility: `hidden` }}
+            >
               {/* {if(spot=== 0){
-                <div className={styles.tapPosStyle} style={{backgroundPosition: `${-30 * tapPos}px, 0px`}}></div>
-              }
-              } */}
+                  <div className={styles.tapPosStyle} style={{backgroundPosition: `${-30 * tapPos}px, 0px`}}></div>
+                }
+                } */}
             </div>
           )),
         )}
